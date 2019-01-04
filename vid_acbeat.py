866f18896a5e7d894bac39bad7798824d1e20c2a
Shubhankar Shubham <44786799+Shubhankar055@users.noreply.github.com>
2019-01-04 23:49:58 +0530

    Update vid_acbeat.py

--- vid_acbeat.py
+++ vid_acbeat.py
@@ -2,8 +2,8 @@
 ## CAN BE UTILISED IN OUR PROJECT TO CUT VIDEO ACCORDING TO BEAT TIMELENGTH
 ##
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip  
-l = [23, 46, 68, 90, 112, 135]      ##TESTING LIST
-vid = ['1234.mp4' , 'abcd.mp4']     ##TESTING VIDEOS
+l = [23, 46, 68, 90, 112, 135]      ##TESTING LIST will implement output length from my beat detector code
+vid = ['1234.mp4' , 'abcd.mp4']     ##TESTING VIDEOS will take input of the videos
 tup = zip(l,vid)
 #for num in range(0,55):
 for tim,vids in tup:
